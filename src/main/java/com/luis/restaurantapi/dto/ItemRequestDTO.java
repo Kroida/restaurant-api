79625b1cd45df9de439ee5d4c5a93c3c0603150a
Luís <luisGNS8022@gmail.com>
2026-09-13 23:26:32 -0300

    Id e datas retiradas

--- src/main/java/com/luis/restaurantapi/dto/ItemRequestDTO.java
+++ src/main/java/com/luis/restaurantapi/dto/ItemRequestDTO.java
@@ -1,7 +1,6 @@
 package com.luis.restaurantapi.dto;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 public record ItemRequestDTO(String name, String description, Integer quantity, BigDecimal price, String imageUrl) {
 }
