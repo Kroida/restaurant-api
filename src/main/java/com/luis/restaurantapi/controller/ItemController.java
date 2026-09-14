61fba09d3b980c1aa02485592f1a8e55f8bce2ae
Luís <luisGNS8022@gmail.com>
2026-09-13 23:22:18 -0300

    Endpoints iniciais

--- src/main/java/com/luis/restaurantapi/controller/ItemController.java
+++ src/main/java/com/luis/restaurantapi/controller/ItemController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/items")
-public class itemController {
+public class ItemController {
     @PostMapping
     public void assignItem() {
         return;
