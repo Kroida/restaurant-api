e849ac561824e82ee6c6dbd59b1004f243546567
Luís <luisGNS8022@gmail.com>
2026-09-13 23:24:26 -0300

    Id e datas retiradas

--- src/main/java/com/luis/restaurantapi/dto/ItemRequestDTO.java
+++ src/main/java/com/luis/restaurantapi/dto/ItemRequestDTO.java
@@ -3,5 +3,5 @@ package com.luis.restaurantapi.dto;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record itemRequestDTO(Long id, String name, String description, Integer quantity, BigDecimal price, String imageUrl, LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record ItemRequestDTO(String name, String description, Integer quantity, BigDecimal price, String imageUrl) {
 }
--- src/main/java/com/luis/restaurantapi/dto/ItemResponseDTO.java
+++ src/main/java/com/luis/restaurantapi/dto/ItemResponseDTO.java
@@ -1,10 +1,12 @@
 package com.luis.restaurantapi.dto;
 
+import com.luis.restaurantapi.model.Item;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record itemResponseDTO(Long id, String name, String description, Integer quantity, BigDecimal price, String imageUrl, LocalDateTime createdAt, LocalDateTime updatedAt) {
-    public itemResponseDTO(Item item) {
-        this(item.getId(), item.getName(), item.getDescription(), item.getQuantity(), item.getPrice(), item.imageUrl(), item.createdAt(), item.updatedAt());
+public record ItemResponseDTO(Long id, String name, String description, Integer quantity, BigDecimal price, String imageUrl, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public ItemResponseDTO(Item item) {
+        this(item.getId(), item.getName(), item.getDescription(), item.getQuantity(), item.getPrice(), item.getImageUrl(), item.getCreatedAt(), item.getUpdatedAt());
     }
 }
